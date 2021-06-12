2762d33039797094e2ff0eca0e2a9eaffccbd9b7
Tugce Nur Tas <tas.tugcenur@gmail.com>
2021-06-12 13:20:42 +0200

    initial commit

---
+++ src/public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHPH Include Exercise</title>
+</head>
+
+<body>
+
+  <?php
+
+  phpinfo();
+
+  echo '<pre>';
+  print_r($_SERVER);
+  echo '</pre>';
+
+  ?>
+
+</body>
+
+</html>
