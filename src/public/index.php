743a212154d5ad113861dd1bf19e1d0766023edd
Tugce Nur Tas <tas.tugcenur@gmail.com>
2021-06-13 10:07:45 +0200

    feat: update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHPH Include Exercise</title>
+  <title>PHP Include Exercise</title>
 </head>
 
 <body>
@@ -13,10 +13,6 @@
   <?php
 
   phpinfo();
-
-  echo '<pre>';
-  print_r($_SERVER);
-  echo '</pre>';
 
   ?>
 
